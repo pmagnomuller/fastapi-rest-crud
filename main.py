9b16c1fc1c403f74a289a7a71b29177a222cce70
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2023-02-02 15:15:25 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from fastapi import Depends, FastAPI
+from database import Base, engine, SessionLocal
+import schemas
+import models
+from sqlalchemy.orm import Session
+
+
+# This will create our database if it doesent already exists
+Base.metadata.create_all(engine)
+
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+fakeDatabase = {
+    1: {'task': 'Clean car'},
+    2: {'task': 'Write blog'},
+    3: {'task': 'Start stream'},
+}
+
+app = FastAPI()
+
+
+@app.get("/")
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
+
+
+@app.get("/{id}")
+def getItem(id: int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
+
+# option #1
+# @app.post("/")
+# def addItem(task:str):
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {"task":task}
+#     return fakeDatabase
+
+# Option #2
+
+
+@app.post("/")
+def addItem(item: schemas.Item, session: Session = Depends(get_session)):
+    item = models.Item(task=item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
+
+# Option #3
+# @app.post("/")
+# def addItem(body = Body()):
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {"task":body['task']}
+#     return fakeDatabase
+
+
+@app.put("/{id}")
+def updateItem(id: int, item: schemas.Item, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    itemObject.task = item.task
+    session.commit()
+    return itemObject
+
+
+@app.delete("/{id}")
+def deleteItem(id: int, session: Session = Depends(get_session)):
+    itemObject = session.query(models.Item).get(id)
+    session.delete(itemObject)
+    session.commit()
+    session.close()
+    return 'Item was deleted...'
